2c30db30012d5daac4fedaa6de59d502460a808b
Rodrigo Palombo <116583410+Palombo-dev@users.noreply.github.com>
2022-12-15 17:53:31 -0300

    ex2

---
+++ inicio/ex02.js
@@ -0,0 +1,12 @@
+var nome = "caneta"
+var preco = 6
+var imposto = 1.5
+var precoFinal = preco + imposto
+
+console.log(nome)
+console.log(preco)
+console.log(imposto)
+console.log(precoFinal)
+
+nome = "CANETA"
+console.log(nome)
