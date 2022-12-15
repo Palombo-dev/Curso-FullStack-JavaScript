22605186b58564dec1156495da34b845b80c2bd2
Rodrigo Palombo <116583410+Palombo-dev@users.noreply.github.com>
2022-12-15 19:15:56 -0300

    modulo 2

---
+++ desafios/desafio02.js
@@ -0,0 +1,7 @@
+//Circunferencia
+
+const PI = 3.14
+let raio = 13
+let circunferencia = PI * (raio * raio)
+
+console.log(`A cincunferencia é ${circunferencia} metros quadrados.`)
---
+++ desafios/desafio03.js
@@ -0,0 +1,16 @@
+//Troca de valores
+
+let a1 = 7
+let b1 = 94
+
+console.log(a1)
+console.log(b1)
+
+
+
+let a2 = b1
+let b2 = a1
+console.log(a2)
+console.log(b2)
+
+//Tambem pode funcionar [a, b] = [b, a]
