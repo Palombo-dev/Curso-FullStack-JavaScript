e9a80e085632b3f1b66d98fbbbae2a8987edfb98
Rodrigo Palombo <116583410+Palombo-dev@users.noreply.github.com>
2022-12-19 10:47:06 -0300

    e

---
+++ funcoes/number.js
@@ -0,0 +1,10 @@
+let a = 2
+let b = "3"
+console.log(a, b)
+console.log(typeof a, typeof Number(b))
+
+let c = 12.2345
+console.log(c.toFixed(0))
+console.log(2.30.toFixed(0))
+console.log(Math.pow(3, 2)) // math.pow() eleva ao quadrado
+
---
+++ inicio/obj.js
@@ -0,0 +1,5 @@
+const prod1 = {}
+console.log(prod1)
+prod1.nome = "Celular-ultra-mega"
+console.log(prod1)
+console.log()
