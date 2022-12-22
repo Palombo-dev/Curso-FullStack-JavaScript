fccae3354089bf2f7a48a5fe080723b4fcd6a9a5
Rodrigo Palombo <116583410+Palombo-dev@users.noreply.github.com>
2022-12-22 19:39:30 -0300

    dasafiojeu

--- desafios/desafiojeu.js
+++ desafios/desafiojeu.js
@@ -1,8 +1,8 @@
 //contar quatos dias de vida a pessoa tem, da data de nascença, até hoje, incluindo anos bixessuaisssss
 const people = {
-    day: 12,
-    month: 9,
-    year: 2003,
+    day: 9,
+    month: 8,
+    year: 2000,
 }
 
 const dateNow = {
@@ -11,22 +11,25 @@ const dateNow = {
     year: 2022,
 }
 
+//calcula numero de anos
 const testY = function (a, b){
     let tester = a - b
     return tester //01
+    
 }
 
+//calcula numero de anos, transforma em dias, 
 const testM = function (){
-    let month = testY(dateNow.year, people.year)
-    month = (month - 2) * 12 + (12 - people.month) + dateNow.month 
-    return month //02
+    let age = testY(dateNow.year, people.year)
+    let months = age * 12 - people.month + dateNow.month - 1
+    return months //02
 }
 
 const testD = function(){
-    let day = testM() * 30 + (testY(dateNow.year, people.year) * 7) - (testY(dateNow.year, people.year) - 5)
+    let day = testM() * 30 + (testY(dateNow.year, people.year) * 7) - (testY(dateNow.year, people.year)) + dateNow.day
     return day //03
 }
-
+//calcula bisexto e reduz do total
 const testB = function(){
     let i = people.year
     let dateB = 0
