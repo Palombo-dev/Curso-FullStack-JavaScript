da855e6045f3088a20b1570da2f7a96090fe3fcc
Rodrigo Palombo <116583410+Palombo-dev@users.noreply.github.com>
2023-01-02 15:42:22 -0300

    ex3 e ex4

---
+++ exercicios.js/ex3.js
@@ -0,0 +1,7 @@
+/*Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.*/
+
+function exponential(a, b){
+    return a ** b
+}
+
+console.log(exponential(9, 2))
