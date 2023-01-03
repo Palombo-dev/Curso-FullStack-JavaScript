ccfabc214f559904b2761d6f4631f989f27a6d48
Rodrigo Palombo <116583410+Palombo-dev@users.noreply.github.com>
2023-01-03 19:48:43 -0300

    post

---
+++ exercicios.js/ex13.js
@@ -0,0 +1,23 @@
+/*Crie um programa que exibe se um dia é dia útil, fim de semana ou dia inválido dado o número referente ao
+dia. Considere que domingo é o dia 1 e sábado é o dia 7. Utilize a estrutura Switch.*/
+
+n = 1
+
+switch(n){
+    case 1: 
+        return console.log('fim de semana')
+    case 2: 
+        return console.log('Dia util')
+    case 3: 
+        return console.log('Dia util')
+    case 4: 
+        return console.log('Dia util')
+    case 5: 
+        return console.log('Dia util')
+    case 6: 
+        return console.log('Dia util')
+    case 7: 
+        return console.log('fim de semana')
+    
+}
+
