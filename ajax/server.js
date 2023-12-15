5a5bd47e6020e6e252132880abd760d185c68c18
Rodrigo Palombo <116583410+Palombo-dev@users.noreply.github.com>
2023-12-15 19:13:54 -0300

    http2

--- ajax/server.js
+++ ajax/server.js
@@ -1,7 +1,27 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const multer = require('multer')
 
+const storage = multer.diskStorage({
+    destination: function (req, file, callback){
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err =>{
+        if(err){
+            return res.send('ocorreu um erro')
+        }
+        res.end('concluido com sucesso')
+    })
+})
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
