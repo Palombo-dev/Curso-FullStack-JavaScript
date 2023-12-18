2b7465201f69a05711d56f141401cd3c952b83b4
Rodrigo Palombo <116583410+Palombo-dev@users.noreply.github.com>
2023-12-18 17:49:13 -0300

    primeira task concluida

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,9 +1,30 @@
 const gulp = require('gulp')
-const series = gulp.series
+const {series, parallel } = require('gulp')
 
-function copiar(cb){
-    console.log('tarefa de copiar')
+
+const antes1= cb => {
+    console.log('tarefa antes 1')
     return cb()
 }
 
-module.exports.default = series(copiar)
+function copiar(cb){
+    gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+        .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+
+function antes2(cb){
+    console.log('tarefa antes 2')
+    return cb()
+}
+
+function fim(cb){
+    console.log('tarefa final')
+    return cb()
+}
+
+module.exports.default = series(
+    parallel( antes1, antes2),
+    copiar,
+    fim,
+)
